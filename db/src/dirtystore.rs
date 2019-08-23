7dcea88bb55c9ea71255371b4e676654e97a12e7
Karl Whitford <karl@blackesquirel.com>
2019-08-23 03:29:43 -0400

    Internal representation of a causetID
    The causet mechanism resolves as follows:
    
    The internal representation of the the causet is embedded in CausetID: 1)Numeric CausetID, 2)lookup-ref (atomized[ARA v ]pair), 3)temporary causet

---
+++ db/src/dirtystore.rs
@@ -0,0 +1,37 @@
+//Copyright 2019 EinsteinDB/Venire Labs Inc All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+use std::collections::HashMap;
+use std::rc::Rc;
+
+//Reference count indexing a tree.
+pub type LookupRef = Rc<ARAPair>;
+
+pub type TempIdHandle = Rc<TempId>;
+pub type TempIdMap = HashMap<TempIdHandle, Entid>;
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialOrd, PartialEq)]
+pub enum Term<E, V> {
+    AddOrRetract(OpType, E, CausetID, V),
+}
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialOrd, PartialEq)]
+pub enum Either<L, R> {
+    Left(L),
+    Right(R),
+}
+
+use self::Either::*;
+
+pub enum LookupRefOrTempId {
+  LookupRef(LookupRef),
+  TempId(TempIdHandle)
+
+}
