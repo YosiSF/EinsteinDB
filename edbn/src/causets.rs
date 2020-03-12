c1d0be37f746d2a110c0b285243d494aea2a781f
CavHack <karl@einsteindbsql.com>
2020-03-11 20:34:16 -0700

    from reference count
    record arrival as the the Arc index of cloned field types.

--- edbn/src/causets.rs
+++ edbn/src/causets.rs
@@ -10,6 +10,20 @@
 
 //! This module defines core types that support the transaction processor.
 
+use std::collections::BTreeMap;
+use std::fmt;
+
+use std::collections::HashSet;
+use std::hash::Hash;
+use std::ops::{
+    Deref,
+    DerefMut,
+};
+
+/*use ::{
+    ValueRc,
+};*/
+
 
 use ::std::rc::{
     Rc,
@@ -18,6 +32,10 @@ use ::std::rc::{
 use ::std::sync::{
     Arc,
 };
+
+
+
+
 
 pub trait FromRc<T> {
     fn from_rc(val: Rc<T>) -> Self;
@@ -35,4 +53,73 @@ impl<T> FromRc<T> for Rc<T> where T: Sized + Clone {
             Err(r) => Self::new(r.cloned()),
         }
     }
+
+    impl<T> FromRc<T> for Arc<T> where T: Sized + Clone {
+       fn from_rc(val: Rc<T>) -> Self {
+           match ::std::rc::Rc::<T>::try_unwrap(val) {
+               Ok(v) => Self::new(v),
+               Err(r) => Self::new(r.cloned()),
+           }
+       }
+
+       fn from_arc(val: Arc<T>) -> Self {
+           val.clone()
+       }
+   }
+
+   impl<T> FromRc<T> for Box<T> where T: Sized + Clone {
+    fn from_rc(val: Rc<T>) -> Self {
+        match ::std::rc::Rc::<T>::try_unwrap(val) {
+            Ok(v) => Self::new(v),
+            Err(r) => Self::new(r.cloned()),
+        }
+    }
+
+    fn from_arc(val: Arc<T>) -> Self {
+        match ::std::sync::Arc::<T>::try_unwrap(val) {
+            Ok(v) => Self::new(v),
+            Err(r) => Self::new(r.cloned()),
+        }
+    }
 }
+
+
+pub trait Cloned<T> {
+    fn cloned(&self) -> T;
+    fn to_value_rc(&self) -> ValueRc<T>;
+}
+
+impl<T: Clone> Cloned<T> for Rc<T> where T: Sized + Clone {
+    fn cloned(&self) -> T {
+        (*self.as_ref()).clone()
+    }
+
+    fn to_value_rc(&self) -> ValueRc<T> {
+        ValueRc::from_rc(self.clone())
+    }
+}
+
+impl<T: Clone> Cloned<T> for Arc<T> where T: Sized + Clone {
+    fn cloned(&self) -> T {
+        (*self.as_ref()).clone()
+    }
+
+    fn to_value_rc(&self) -> ValueRc<T> {
+        ValueRc::from_arc(self.clone())
+    }
+}
+
+impl<T: Clone> Cloned<T> for Box<T> where T: Sized + Clone {
+    fn cloned(&self) -> T {
+        self.as_ref().clone()
+    }
+
+    fn to_value_rc(&self) -> ValueRc<T> {
+        ValueRc::new(self.cloned())
+    }
+}
+
+//type alias with Cloned
+//Type must implement FromRc and Cloned
+//With a From implementation evistant.
+pub type ValueRc<T> = Arc<T>;
