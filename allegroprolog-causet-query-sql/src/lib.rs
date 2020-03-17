f4fbc87b118e11c94ca7983fae3fcd6928274a96
CavHack <karl@einsteindbsql.com>
2020-03-16 23:51:30 -0700

    Implicit prep
    (Implicit) preparation: creating views, tables, indices, or even whole databases or connections upon which the query will be run.
    
    Co-Authored-By: slushie <slushie@users.noreply.github.com>

--- allegroprolog-causet-query-sql/src/lib.rs
+++ allegroprolog-causet-query-sql/src/lib.rs
@@ -26,3 +26,38 @@ impl From<QueryValue> for ColumnOrExpression {
         }
     }
 }
+
+// Short-hand for a list of tables all inner-joined.
+pub struct TableList(pub Vec<TableOrSubquery>);
+
+impl TableList {
+    fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+}
+
+pub struct Join {
+    left: TableOrSubquery,
+    op: JoinOp,
+    right: TableOrSubquery,
+    // TODO: constraints (ON, USING).
+}
+
+#[allow(dead_code)]
+pub enum TableOrSubquery {
+    Table(SourceAlias),
+    Union(Vec<SelectQuery>, TableAlias),
+    Subquery(Box<SelectQuery>),
+    Values(Values, TableAlias),
+}
+
+pub enum Values {
+    /// Like "VALUES (0, 1), (2, 3), ...".
+    /// The vector must be of a length that is a multiple of the given size.
+    Unnamed(usize, Vec<TypedValue>),
+
+    /// Like "SELECT 0 AS x, SELECT 0 AS y WHERE 0 UNION ALL VALUES (0, 1), (2, 3), ...".
+    /// The vector of values must be of a length that is a multiple of the length
+    /// of the vector of names.
+    Named(Vec<Variable>, Vec<TypedValue>),
+}
