1e97061912b36f7af921146b8a8d6946496a33dd
Tyler Foehr <karl@einsteindbsql.com>
2021-11-08 16:18:44 -0800

    if let Some((timestamp3)) = spacelike[3].0{}
    //if the item exists we replace it with a new one (we dont want a set here because we are only adding single items to the map.) We need to update spacelike and timelike here!
    
    Co-Authored-By: EinsteinDB <71530574+whtcorpsinc@users.noreply.github.com>
    Co-Authored-By: EinsteinDB <46835245+yosi-sf@users.noreply.github.com>

--- einsteindb/src/aries.rs
+++ einsteindb/src/aries.rs
@@ -1,4 +1,4 @@
-//Copyright 2020 WHTCORPS INC
+//Copyright 2021 WHTCORPS INC
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use
 // this file except in compliance with the License. You may obtain a copy of the
@@ -51,7 +51,7 @@ pub struct TimestepEvaluation<K, V> {
 impl<K, V> Default for TimestepEvaluation<K, V> where K: Ord {
     fn default() -> TimestepEvaluation<K, V> {
         TimestepEvaluation
-     {
+        {
             lightlike: BTreeMap::default(),
             spacelike: BTreeMap::default(),
             timelike: BTreeMap::default(),
@@ -74,6 +74,34 @@ impl<K, V> Default for TimestepEvaluation<K, V> where K: Ord {
                 }
             }
         }
-
-
+    }
 }
+
+impl<K, V> TimestepEvaluation<K, V> where K: Ord {
+    pub fn add_lightlike(&mut self, key: K, value: V) {
+        self.lightlike.insert(key, value);
+    }
+
+    pub fn remove_lightlike(&mut self, key: &K) -> Option<V> {
+        self.lightlike.remove(key)
+    }
+
+    pub fn get_lightlike(&self, key: &K) -> Option<&V> {
+        self.lightlike.get(key)
+    }
+
+    pub fn remove_spacelike(&mut self, key: &K) -> Option<V> {
+        self.spacelike.remove(key)  //returns None if the key is not present in the map (einsteindb), this does not mean that we can have a empty map!
+
+    }
+
+    pub fn add_spacelike(&mut self, key: K, value: V) { //if the item exists we replace it with a new one (we dont want a set here because we are only adding single items to the map.) We need to update spacelike and timelike here!
+        let _ = self.spacelike.insert(key,value); //add an element if it doesnt exist and return true or false if it did exist before... also returns old value!
+        //
+        // /* if let Some((t
+        // /* if let Some((timestamp2)) = spacelike[1].0{} */ //then check against timestamp2...
+        // they should be different!!!
+        //
+        // so we can simply add them!!! even though its a set data structure it will not allow duplicates!! :) This will work for any number of elements in our case spacialist and lightlist are both sets which means that duplicates cannot be added - thats good for us!! :) :) :) :) :) :) imestamp1)) = spacelike[0].0{} */ //take out first tuple... and assign its 0 to timestamp1... then check against current timestamp1 in spacelike[0] index! is there any other values??? yes see below...
+
+
