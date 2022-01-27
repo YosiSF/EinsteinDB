c2716bf10458ae06565a22271ab808e5a6c91b46
Karl Whitford <71530574+whtcorpsinc@users.noreply.github.com>
2022-01-27 07:36:05 -0800

    All maps must be disjoint except for possibly unique attributes on either side but at least one side must have unique attributes
    Given two sets of Causetids and two sets of Solitonids, return an iterator over all possible pairs of (Causets A, Causets B, Solitasn A*, Solitands B*) such that `get_by_right` yields `A`, `get_byleft` yields `B`, `delete*` removes only `A`, and `insert*` inserts only `B`.  Also accepts attrmaps with optional attributes on either side that don't correspond to causal relationships between components or between components themselves but do exist on one side or another side specifically for identifying some purpose (like distinguishing a candidate Causal Set from others--e.g., something like 'this is an index I'm creating for search purposes'). All maps must be disjoint except for possibly unique attributes on either side but at least one side must have unique attributes; iow you can't make three different cs's for three different queries based on unique user-provided data on one side but not the other like foo=value1 foo=value2 foo=value3 vs bar=value1 bar=value2 bar=value3... It would make sense to add an option later which forces uniqueness so I can use what I want without having to worry about name collisions whenever I'm being sloppy...which is almost always since I'm so rarely working with more than four elements at once but still that is my expectation right now although clearly its more generalizable to just let me define multiple query

--- einsteindb-gen/embedded/src/lib.rs
+++ einsteindb-gen/embedded/src/lib.rs
@@ -121,35 +121,111 @@ pub struct Topograph {
 
 }
 
-pub trait HasTopograph {
-    fn causetid_for_type(&self, t: ValueType) -> Option<KnownCausetid>;
-
-    fn get_solitonid<T>(&self, x:T) -> Option<&Keyword> where T: Into<Causetid>;
-    fn get_causetid(&self, x: &Keyword) -> Option<KnownCausetid>;
-    fn Attr_for_causetid<T>(&self, x: T) -> Option<&Attr> where T: Into<Causetid>;
-
-        // Returns the Attr and the causetid named by the provided solitonid.
-        fn Attr_for_ident(&self, solitonid: &Keyword) -> Option<(&Attr, Knowncausetid)>;
-
-        /// Return true if the provided causetid identifies an Attr in this topograph.
-        fn is_Attr<T>(&self, x: T) -> bool where T: Into<Causetid>;
-
-        /// Return true if the provided solitonid identifies an Attr in this topograph.
-        fn identifies_Attr(&self, x: &Keyword) -> bool;
-
-        fn component_Attrs(&self) -> &[Causetid];
-
-
-}
-
 impl Topograph {
-    pub fn new(solitonid_map: , causetid_map: causetidMap, Attr_map: AttrMap) -> Topograph {
-        let mut s = Topograph { solitonid_map, causetid_map, Attr_map, component_Attrs: Vec::new() };
-        s.update_component_Attrs();
-        s
+    /// `rel`: The attribute on which to find an existing component.
+    pub fn get_component(&self, rel: Causetid) -> Vec<Causetid> {
+        return vec![];
     }
 
-    pub fn to_edn_value(&self) -> edn::Value {
+    // Should return a vector representing the components of each solitonid. If a solitonid is not in the topograph, it should be treated as if it were in a singleton component. A solitonid may appear in multiple connected components.  For example: Given the following topograph (where (1) represents a causetid, and <foo> represents a solitonid):
+    // ```clojure
+    //   (ba/subset #{(<foo> 1)} '(#{(<foo> 1), (<bar> 2)})) ;;=> #{((<foo> 1), (<bar> 2))}
+    //   (ba/subset #{(<foo> 1)} '(#{(<foo> 1), (<bar> 3)})) ;;=> #{((<foo> 1), (<bar> 3))}
+    //   (ba/subset #{(<hello>) 2} '#{(1 2)} )               ;;=>  #{((1 2), (<hello>) 2)}
+
+    pub fn diff_components(&self) -> TopographDiffIterator {
+        let new_topo = Topograph { ..Default::default() };
+
+        Box::new(TopographDiffIterator::new(&new_topo, self));
+
+        let mut comps = HashMap::new();
+        for ctida in 0..self._causetids.len() + 1 {
+            comps.insert();
+        };
+
+        for node in topspec.nodes().iter() {};
+
+        let mut v = Vec::new();
+        for trait_ref in trait_refs {
+            v.push()
+        };
+
+        println!("New nodes:\n");
+
+        for node in topspec.nodes().iter() { println!(" {:?}", node); };
+
+        println!("\nNew edges:\n");
+        for edge in topspec.edges().iter() {
+            println!(" {:?}", edge);
+        };
+    }
+
+        // ...return bimap diff result...;//diff(self, new_map);
+        // bimap diff...;//topspec diff...;
+        // difference between old and new topo...;//TO-DO...;/*println!("***diff***\nOld: {}\nNew: {}\n***diff***", self, new_map);*///TO-DO...;///TO-DO...;///TO-DO...;///TO-DO...;///TO-DO..."}, {"0"}));//FIXME!!!/////TO-DO..."}, {"0"}));//FIXME!!!/////TO-DO..."}, {"0"}));//FIXME!!!/////TO-DO..."}, {"0"}));//FIXME!!!/////TO-DO..."}, {"0"}));//FIXME!!!/////TODO..."}, {"0"}));//FIXME!!!/////TODO..."}, {"0"}));//FIXME!!!/////TODO..."}, {"0"}));//FIXME!!!/////TODO..."}, {"0"}));//FIXME!!!/////
+        /// `rel`: The attribute on which to find an existing component.
+
+
+            // Should return a vector representing the components of each solitonid. If a solitonid is not in the topograph, it should be treated as if it were in a singleton component. A solitonid may appear in multiple connected components.  For example: Given the following topograph (where (1) represents a causetid, and <foo> represents a solitonid):
+            // ```clojure
+            //   (ba/subset #{(<foo> 1)} '(#{(<foo> 1), (<bar> 2)})) ;;=> #{((<foo> 1), (<bar> 2))}
+            //   (ba/subset #{(<foo> 1)} '(#{(<foo> 1), (<bar> 3)})) ;;=> #{((<foo> 1), (<bar> 3))}
+            //   (ba/subset #{(<hello>) 2} '#{(1 2)} )               ;;=>  #{((1 2), (<hello>) 2)}
+
+            //pub fn diff_components(&self) -> TopographDiffIterator {
+    //let new_topo = Topograph { ..Default::default() };
+
+                //Box::new(TopographDiffIterator::new(&new_topo, self));
+
+                pub fn new(solitonid_map: , causetid_map: causetidMap, attr_map: AttrMap) -> Topograph {
+                    let mut s = Topograph { solitonid_map, causetid_map, Attr_map: attr_map, component_Attrs: Vec::new() };
+                    s.update_component_Attrs();
+                    s
+                }
+
+                pub fn update_component_Attrs(&mut self) {
+                    for (k, v) in self.causetid_map.iter() {
+                        if v == &Attribute {
+                            self.component_Attrs.push(k);
+                        }
+                    }
+                }
+
+                pub fn print(&self) {}
+            }
+
+
+            // Given a topograph and a map from solitonid to new causetids, creates a new topograph where each solitonid has been replaced by the corresponding new causetids and updates the rels with the new added causers and rels.
+            // The old solitonids are left as-is; they are not deleted or modified in any way. This method will fail if it attempts to replace a solitonid that does not exist in the old topograph or if it attempts to create duplicate causetids for a solitonid pair. For example: Given the following topograph (where (1) represents a causetid):
+            // TopographIdentity(#{((<foo> 1), (<bar> 2)) ((<foo>) 2)} :a/bang #{<baz> 3 <qux> 4} "hello") ;;=> TopographIdentity(#{((<foo>) 1 :a/bang ((<bar>) 2) "hello")} :a/bang #{((<baz>) 3) (<qux>) 4} "hello")
+
+            // Given two topographs, returns true if both have the same set of solitons. Obeys set equality rules. For example:
+            // TopoCompare(Topography{..Default::default()},Topography{..Default::default()}) ;;=> true
+            // TopoCompare(Topography{..Default::default()},Topography{solitonid_map: ..Default::default(), causetid_map: ..Default::default(), Attr_map: ..Default::default(), component_Attrs: ..Default::default()}) ;;=> false
+
+            // Given two sets of Causetids and two sets of Solitonids, return an iterator over all possible pairs of (Causets A, Causets B, Solitasn A*, Solitands B*) such that `get_by_right` yields `A`, `get_byleft` yields `B`, `delete*` removes only `A`, and `insert*` inserts only `B`.  Also accepts attrmaps with optional attributes on either side that don't correspond to causal relationships between components or between components themselves but do exist on one side or another side specifically for identifying some purpose (like distinguishing a candidate Causal Set from others--e.g., something like 'this is an index I'm creating for search purposes'). All maps must be disjoint except for possibly unique attributes on either side but at least one side must have unique attributes; iow you can't make three different cs's for three different queries based on unique user-provided data on one side but not the other like foo=value1 foo=value2 foo=value3 vs bar=value1 bar=value2 bar=value3... It would make sense to add an option later which forces uniqueness so I can use what I want without having to worry about name collisions whenever I'm being sloppy...which is almost always since I'm so rarely working with more than four elements at once but still that is my expectation right now although clearly its more generalizable to just let me define multiple query
+pub trait HasTopograph {
+                fn causetid_for_type(&self, t: ValueType) -> Option<KnownCausetid>;
+
+                fn get_solitonid<T>(&self, x: T) -> Option<&Keyword> where T: Into<Causetid>;
+                fn get_causetid(&self, x: &Keyword) -> Option<KnownCausetid>;
+                fn Attr_for_causetid<T>(&self, x: T) -> Option<&Attr> where T: Into<Causetid>;
+
+                // Returns the Attr and the causetid named by the provided solitonid.
+                fn Attr_for_ident(&self, solitonid: &Keyword) -> Option<(&Attr, Knowncausetid)>;
+
+                /// Return true if the provided causetid identifies an Attr in this topograph.
+                fn is_Attr<T>(&self, x: T) -> bool where T: Into<Causetid>;
+
+                /// Return true if the provided solitonid identifies an Attr in this topograph.
+                fn identifies_Attr(&self, x: &Keyword) -> bool;
+
+                fn component_Attrs(&self) -> &[Causetid];
+
+
+
+
+    fn to_edn_value(&self) -> edn::Value {
         edn::Value::Vector((&self.Attr_map).iter()
             .map(|(causetid, Attr)|
                 Attr.to_edn_value(self.get_ident(*causetid).cloned()))
@@ -160,7 +236,7 @@ impl Topograph {
         self.solitonid_map.get(x).map(|x| *x)
     }
 
-    pub fn update_component_Attrs(&mut self) {
+    fn update_component_Attrs(&mut self) {
         let mut components: Vec<Causetid>;
         components = self.Attr_map
                          .iter()
