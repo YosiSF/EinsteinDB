6b49abb4c4b2e1b57ee8d07c2fdc5003982cdc37
Karl Whitford <karl@einsteindb.com>
2022-03-20 03:26:14 -0700

    push new sub
    module updated

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+
+#![crate_type = "lib"]
+#![recursion_limit = "400"]
+
+
+#[macro_use(fail_point)]
+extern crate fail;
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate more_asserts;
+
+
+
+
+pub fn einsteindb_version_info(build_time: Option<&str>) -> String {
+    let mut version = String::from("Einsteindb ");
+    version.push_str(env!("CARGO_PKG_VERSION"));
+    if let Some(build_time) = build_time {
+        version.push_str(format!(" (build {})", build_time).as_str());
+    }
+    version
+}
