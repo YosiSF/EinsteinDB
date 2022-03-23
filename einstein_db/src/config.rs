919f88159105de767cd9ac52b9a45b01f15d3327
Karl Whitford <karl@einsteindb.com>
2022-03-23 04:49:18 -0700

    we tuplefy nested clojars which are suffixed byte strings appended to the lexicographic prefix bloom filter, the trick is to save a snapshot of the tabular representation without persisting the block linearly, instead 'it must ask itself, will this be needed in the next hour, or should i  keep it in the cache for seven hours'

--- einstein_db/src/config.rs
+++ einstein_db/src/config.rs
@@ -1,8 +1,10 @@
-
 //! Configuration for the entire server.
 //!
 //! EinsteinDB is configured through the `EinsteinDBConfig` type, which is in turn
 //! made up of many other configuration types.
+
+
+
 
 use std::cmp;
 use std::collections::HashMap;
@@ -21,26 +23,32 @@ use einstein_db_util::sys::SysQuota;
 use einstein_db_util::duration_to_sec;
 use einstein_db_util::yatp_pool;
 
-//Block cache for FoundationDB whose size will be set to 45% of system memory
-pub const HYPERCAUSET_CACHE_RATE: f64 = 0.45;   
-//BY default, EinsteinDB will try to limit memory usage to 75% of sys mem.__rust_force_expr!
 
-const HYPERINTERLOCKED_CAUSETS=_MIN_MEM: usize = 256 * MIB as usize;
+
+//Block cache for FoundationDB whose size will be set to 45% of system memory
+
+
+pub const HYPERCAUSET_CACHE_RATE: f64 = 0.45;
+//BY default, EinsteinDB will try to limit memory usage to 75% of sys mem.__rust_force_expr!
+const HYPERINTERLOCKED_CAUSETS: () =_MIN_MEM: usize = 256 * MIB as usize;
 const HYPERINTERLOCKED_CAUSETS_MAX_MEM: usize = 1024 * GIB as usize;
 const LAST_CONFIG_FILE: &str = "last_einsteindb.toml";
 const TMP_CONFIG_FILE: &str = "tmp_einsteindb.toml";
 const MAX_HYPERCAUSET_SIZE: usize = 1024 * GIB as usize;
 
 fn memory_limit_for_hyyperinterlocked_causets(sys_mem: usize) -> usize {
+
     let mut limit = (sys_mem as f64 * HYPERCAUSET_CACHE_RATE) as usize;
+
+
+
     if limit < HYPERINTERLOCKED_CAUSETS {
+
         limit = HYPERINTERLOCKED_CAUSETS;
     }
+
     if limit > HYPERINTERLOCKED_CAUSETS_MAX_MEM {
-        limit = HYPERINTERLOCKED_CAUSETS_MAX_MEM;
-    }
-    limit
-};  
+
     let (ratio, min, max) = match(
         config::get_config_value_as_f64(&config, "hypercauset_cache_rate"),
         config::get_config_value_as_usize(&config, "hypercauset_cache_min"),
@@ -50,19 +58,33 @@ fn memory_limit_for_hyyperinterlocked_causets(sys_mem: usize) -> usize {
         _ => (HYPERCAUSET_CACHE_RATE, HYPERINTERLOCKED_CAUSETS, HYPERINTERLOCKED_CAUSETS_MAX_MEM),
     };
     let limit = (sys_mem as f64 * ratio) as usize;
+
+
     if limit < min {
         limit = min;
     }
     if limit > max {
+
         limit = max;
     }
+
     limit
-}   //! The configuration for the entire server.
+
+   //! The configuration for the entire server.
 //!
 //! EinsteinDB is configured through the `EinsteinDBConfig` type, which is in turn
 //! made up of many other configuration types.
 //!                         
-//!                 
-//!                     
+
+
+
+
+
+
+//! The configuration for the entire server.
+//!
+//! EinsteinDB is configured through the `EinsteinDBConfig` type, which is in turn
+//! made up of many other configuration types.  The `EinsteinDBConfig` type is  used to   configure the entire server.
+//!
 //!                     
 //!                                 
