5698906b904cf474641110d3d12f7846a84106d4
Karl Whitford <karl@einsteindb.com>
2022-04-21 14:14:52 -0700

    Wrap the Tuplefied index byte slice from the tree with a semantic encoded hexadecimal solitonid and causet_locale parsing function

--- allegro_poset/src/alexandrov_process.rs
+++ allegro_poset/src/alexandrov_process.rs
@@ -1,9 +1,103 @@
 // Copyright 2019 EinsteinDB Project Authors. Licensed under Apache-2.0.
 
 use fdb_traits::{Mutable, Result, WriteBatch, WriteBatchExt, WriteOptions};
+use std::collections::HashMap;
+use std::sync::Arc;
 
-use crate::fdb_lsh_treePaniceinstein_merkle_tree;
+//now we abstract the lshp-tree into a poset so that it is lisp-like
+//source: https://franz.com/support/documentation/ansicl.94/section/dictio19.htm
 
+
+//the poset is a tree with a root and a set of leaves
+//the leaves are the nodes of the poset
+//the root is the root of the tree
+//the root is the root of the poset
+//the leaves are the leaves of the poset
+//the leaves are the leaves of the tree
+//the leaves are the nodes of the tree
+
+
+//we shall use guillaume's interpretation of a stateless hash tree as featured on the NIST2019 paper
+//SPHINCS-Gravity and its rust implementations
+//
+
+pub struct Poset {
+
+    fdb: Arc<dyn Mutable>,
+
+    //the root of the tree
+    root: Arc<PosetNode>,
+    //the leaves of the tree
+    leaves: HashMap<u64, Arc<PosetNode>>,
+    //sqlite secondary index
+    soft_index: HashMap<u64, Arc<PosetNode>>,
+
+}
+
+#[derive(Clone, Debug)]
+pub struct AllegroPosetAlexandrovProcess {
+    //the root of the tree
+    root: Arc<PosetNode>,
+    //the leaves of the tree
+    leaves: HashMap<u64, Arc<PosetNode>>,
+    //the leaves of the poset
+    poset_leaves: HashMap<u64, Arc<PosetNode>>,
+    //the leaves of the tree
+    tree_leaves: HashMap<u64, Arc<PosetNode>>,
+    //the leaves of the tree
+    tree_leaves_reverse: HashMap<u64, Arc<PosetNode>>,
+}
+
+
+pub struct AlexandrovProcess<K, V> {
+    pub(crate) db: Arc<dyn Mutable>,
+    pub(crate) prefix: Vec<u8>,
+    pub(crate) key_type: K,
+    pub(crate) value_type: V,
+    pub(crate) write_options: WriteOptions,
+
+}
+
+//now we abstract the lshp-tree into a poset so that it is lisp-like
+//source: https://franz.com/support/documentation/ansicl.94/section/dictio19.htm
+
+
+
+
+
+pub fn create_alexandrov_process<K, V>(db: Arc<dyn Mutable>, prefix: Vec<u8>, key_type: K, value_type: V, write_options: WriteOptions) -> Result<AlexandrovProcess<K, V>> {
+    let alexandrov_process = AlexandrovProcess {
+        db,
+        prefix,
+        key_type,
+        value_type,
+        write_options,
+    };
+    Ok(alexandrov_process)
+}
+
+pub fn create_poset(db: Arc<dyn Mutable>, prefix: Vec<u8>, write_options: WriteOptions) -> Result<Poset> {
+    let poset = Poset {
+        fdb: db,
+        root: Arc::new(PosetNode::new(db, prefix, write_options)?),
+        leaves: HashMap::new(),
+        soft_index: HashMap::new(),
+    };
+    Ok(poset)
+}
+
+async fn create_alexandrov_process_async<K, V>(db: Arc<dyn Mutable>, prefix: Vec<u8>, key_type: K, value_type: V, write_options: WriteOptions) -> Result<AlexandrovProcess<K, V>> {
+    let alexandrov_process = AlexandrovProcess {
+        db,
+        prefix,
+        key_type,
+        value_type,
+        write_options,
+    };
+    Ok(alexandrov_process)
+}
+
+/*
 impl WriteBatchExt for Paniceinstein_merkle_tree {
     type WriteBatch = PanicWriteBatch;
     type WriteBatchVec = PanicWriteBatch;
@@ -84,3 +178,34 @@ impl Mutable for PanicWriteBatch {
         panic!()
     }
 }
+*/
+
+//Wrap the Tuplefied index byte slice from the tree with a semantic encoder hexadecimal soliton
+//id and causet_locale
+
+
+pub fn wrap_index_bytes(index_bytes: &[u8], soliton_id: &[u8], causet_locale: &[u8]) -> Vec<u8> {
+    let mut index_bytes_vec = index_bytes.to_vec();
+    index_bytes_vec.extend_from_slice(soliton_id);
+    index_bytes_vec.extend_from_slice(causet_locale);
+    index_bytes_vec
+}
+
+
+pub fn unwrap_index_bytes(index_bytes: &[u8]) -> (&[u8], &[u8]) {
+    let mut index_bytes_vec = index_bytes.to_vec();
+    let soliton_id = index_bytes_vec.split_off(index_bytes.len());
+    let causet_locale = index_bytes_vec;
+    (soliton_id, causet_locale)
+}
+
+
+
+
+
+
+
+
+
+
+
--- einstein_ml/src/ast.rs
+++ einstein_ml/src/ast.rs
@@ -1,4 +1,161 @@
 //ast for pushdown automata from foundation db key-value to gremlin tinkerpop
+//einsteinml is a meta language inspired by allegrocl code generation
+
+
+//! # AST
+//!
+//! The AST is the core of the compiler. It is a tree of nodes that represents the
+//! program. Each node has a type and a list of children. The type of a node is
+//! either a primitive type (e.g. `int`) or a user-defined type (e.g. `Foo`).
+//!     let foo = 5;
+//!    let bar = true;
+//!   let baz = "test";
+//!
+
+
+use std::fmt;
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::collections::VecDeque;
+use std::collections::BTreeMap;
+use std::collections::BTreeSet;
+use std::collections::LinkedList;
+use std::collections::BinaryHeap;
+use std::collections::HashMap;
+use std::collections::BTreeMap;
+use std::collections::BTreeSet;
+use std::collections::LinkedList;
+
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+
+
+pub type Solitonid = String;
+pub type Type = String;
+pub type TypeId = String;
+pub type TypeName = String;
+
+pub fn type_name(name: &str) -> TypeName {
+    format!("{}", name)
+}
+
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum TypeKind {
+    Primitive(Type),
+    UserDefined(TypeName),
+}
+
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum TypeRef {
+    Primitive(Type),
+    UserDefined(TypeName),
+}
+
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord, Copy)]
+pub enum PrimitiveType {
+    Int,
+    Causetid,
+    Solitonid,
+    Float,
+    AEVTrie,
+    AEVTrieNode,
+    Bool,
+    String,
+    Char,
+    Unit,
+    Option(Box<TypeRef>),
+    List(Box<TypeRef>),
+    Map(Box<TypeRef>, Box<TypeRef>),
+    Set(Box<TypeRef>),
+    Tuple(Vec<TypeRef>),
+}
+
+
+
+
+
+//! The AST is constructed by the parser and is then type checked by the type
+//! checker.
+//!
+//! ## Nodes
+//!
+
+//dag nodes with line graph edges
+//is a causet dag with poset coset and solitonid
+
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum Node {
+    //program
+    Program(Vec<Statement>),
+    //statement
+    Statement(Statement),
+    //expression
+    Expression(Expression),
+    //declaration
+    Declaration(Declaration),
+    //type
+    Type(Type),
+    //type declaration
+    TypeDeclaration(TypeDeclaration),
+    //type alias
+    TypeAlias(TypeAlias),
+    //type parameter
+
+    //type parameter constraint list
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum Node {
+    // Statements
+    Let(Solitonid, Box<Node>),
+    LetRec(Solitonid, Vec<Solitonid>, Vec<Node>, Box<Node>),
+    LetTuple(Vec<Solitonid>, Vec<Node>, Box<Node>),
+    LetTupleRec(Vec<Solitonid>, Vec<Node>, Vec<Node>, Box<Node>),
+    LetTupleStruct(Vec<Solitonid>, Vec<Node>, Box<Node>),
+
+    // Expressions
+    Literal(Literal),
+    Solitonid(solitonid),
+    Causetid(causetid),
+
+    // Type
+    Type(Type),
+    TypeName(TypeName),
+    TypeRef(TypeRef),
+    TypeKind(TypeKind),
+    TypeKinds(Vec<TypeKind>),
+    TypeRefs(Vec<TypeRef>),
+
+    // Patterns
+    Pattern(Pattern),
+    PatternName(Solitonid),
+
+}
+
+
+
+
+
+
+
+//! The AST is a tree of nodes. Each node represents a statement or expression.
+//! The node type is either `Statement` or `Expression`.
+//!     let foo = 5;
+//!   let bar = true;
+//! let baz = "test";
+//!
+//! ## Statements
+//!
+//! Statements are the main part of the AST. They represent actions that can be
+//! performed on the program.
 
 use std::collections::HashMap;
 use std::collections::HashSet;
