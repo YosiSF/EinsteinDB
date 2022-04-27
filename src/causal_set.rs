e000c2774aa53768a92526c0cd1ba6bbd7c07930
Karl Whitford <karl@einsteindb.com>
2022-04-27 03:15:42 -0700

    An `CausalSet` allows to "causal_set" some potentially large causet_locales, maintaining a single causet_locale

--- src/causal_set.rs
+++ src/causal_set.rs
@@ -9,6 +9,58 @@
 // specific language governing permissions and limitations under the License.
 
 #![allow(dead_code)]
+#![allow(unused_imports)]
+
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::collections::VecDeque;
+use std::fmt;
+use std::hash::Hash;
+use std::hash::Hasher;
+use std::iter::FromIterator;
+use std::iter::Iterator;
+use std::iter::Peekable;
+use std::ops::Index;
+use std::ops::IndexMut;
+use std::ops::Range;
+use std::ops::RangeFull;
+
+use einstein_ml::{};
+use einstein_db::Causetid;
+use einstein_db::CausetidSet;
+use allegro_poset::{};
+use allegro_poset::{Poset, PosetNode};
+use causet::{Causet, CausetNode};
+use causet::{CausetNodeId, CausetNodeIdSet};
+use causet::{CausetId, CausetIdSet};
+use causet::{CausetIdVec, CausetIdVecSet};
+
+use causetq::{CausetQ, CausetQNode};
+use causetq::{CausetQNodeId, CausetQNodeIdSet};
+use causetq::{CausetQId, CausetQIdSet};
+
+use berolina_sql::{BerolinaSql, BerolinaSqlNode};
+use berolina_sql::{BerolinaSqlNodeId, BerolinaSqlNodeIdSet};
+
+use soliton::{Soliton, SolitonNode};
+use soliton_panic::{SolitonNodeId, SolitonNodeIdSet};
+
+use einstein_db_ctl::{EinsteinDbCtl, EinsteinDbCtlNode};
+use einsteindb_server::{EinsteinDbServer, EinsteinDbServerNode};
+
+use capnproto::{MessageReader, MessageBuilder, Reader, Builder};
+use gremlin::{Gremlin, GremlinNode};
+use gremlin::{GremlinNodeId, GremlinNodeIdSet};
+use FoundationDB::{FdbCluster, FdbClusterNode};
+use FoundationDB::{FdbClusterNodeId, FdbClusterNodeIdSet};
+use FoundationDB::{FdbDatabase, FdbDatabaseNode};
+use Postgres::{Postgres, PostgresNode};
+use Postgres::{PostgresNodeId, PostgresNodeIdSet};
+use InnovationDB::{InnovationDb, InnovationDbNode};
+use InnovationDB::{InnovationDbNodeId, InnovationDbNodeIdSet};
+use KV::{KV, KVNode};
+use KV::{KVNodeId, KVNodeIdSet};
+
 
 use std::collections::HashSet;
 use std::hash::Hash;
@@ -17,47 +69,127 @@ use std::ops::{
     DerefMut,
 };
 
-use ValueRc;
+pub const LOCK_FREE_GC: bool = true;
 
-/// An `CausalSet` allows to "causal_set" some potentially large causet_locales, maintaining a single causet_locale
-/// instance owned by the `CausalSet` and leaving consumers with lightweight ref-counted handles to
-/// the large owned causet_locale.  This can avoid expensive clone() operations.
+pub const LOCK_FREE_GC_NODE_COUNT: usize = 100;
+
+pub const LOCK_FREE_GC_NODE_COUNT_MAX: usize = 100;
+
+///! A Causet is a collection of causet nodes.
 ///
-/// In EinsteinDB, such large causet_locales might be strings or arbitrary [a v] pairs.
+/// # Examples
 ///
-/// See https://en.wikipedia.org/wiki/String_causal_seting for discussion.
-#[derive(Clone, Debug, Default, Eq, PartialEq)]
-pub struct CausalSet<T> where T: Eq + Hash {
-    inner: HashSet<ValueRc<T>>,
+/// ```
+/// use EinsteinDB::{Causet, CausetNode};
+///  use EinsteinDB::{CausetId, CausetIdSet};
+///  use EinsteinDB::{CausetNodeId, CausetNodeIdSet};
+///
+/// let mut causet = Causet::new();
+///  let mut causet_node = CausetNode::new();
+///
+/// causet_node.set_id(CausetNodeId::new(1));
+/// causet_node.set_causet_id(CausetId::new(1));
+///
+/// causet.add_node(causet_node);
+///
+/// assert_eq!(causet.get_node(CausetNodeId::new(1)).unwrap().get_id(), CausetNodeId::new(1));
+/// ```
+pub enum CausetNodeIdSet {
+    CausetIdSet(CausetIdSet),
+    CausetIdVecSet(CausetIdVecSet),
+    CausetQNodeIdSet(CausetQNodeIdSet),
+    BerolinaSqlNodeIdSet(BerolinaSqlNodeIdSet),
+    GremlinNodeIdSet(GremlinNodeIdSet),
+    FdbClusterNodeIdSet(FdbClusterNodeIdSet),
+    FdbDatabaseNodeIdSet(FdbDatabaseNodeIdSet),
+    PostgresNodeIdSet(PostgresNodeIdSet),
+    InnovationDbNodeIdSet(InnovationDbNodeIdSet),
+    KVNodeIdSet(KVNodeIdSet),
+    EinsteinDbCtlNodeIdSet(EinsteinDbCtlNodeIdSet),
+    EinsteinDbServerNodeIdSet(EinsteinDbServerNodeIdSet),
+    SolitonNodeIdSet(SolitonNodeIdSet),
 }
 
-impl<T> Deref for CausalSet<T> where T: Eq + Hash {
-    type Target = HashSet<ValueRc<T>>;
 
-    fn deref(&self) -> &Self::Target {
-        &self.inner
+
+
+impl CausetNodeIdSet {
+    pub fn new() -> CausetNodeIdSet {
+        CausetNodeIdSet::CausetIdSet(CausetIdSet::new())
     }
-}
 
-impl<T> DerefMut for CausalSet<T> where T: Eq + Hash {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.inner
-    }
-}
-
-impl<T> CausalSet<T> where T: Eq + Hash {
-    pub fn new() -> CausalSet<T> {
-        CausalSet {
-            inner: HashSet::new(),
+    pub fn add(&mut self, node_id: CausetNodeId) {
+        match self {
+            CausetNodeIdSet::CausetIdSet(causet_id_set) => {
+                causet_id_set.add(node_id.get_causet_id());
+            },
+            CausetNodeIdSet::CausetIdVecSet(causet_id_vec_set) => {
+                causet_id_vec_set.add(node_id.get_causet_id_vec());
+            },
+            CausetNodeIdSet::CausetQNodeIdSet(causet_q_node_id_set) => {
+                causet_q_node_id_set.add(node_id.get_causet_q_node_id());
+            },
+            CausetNodeIdSet::BerolinaSqlNodeIdSet(berolina_sql_node_id_set) => {
+                berolina_sql_node_id_set.add(node_id.get_berolina_sql_node_id());
+            },
+            CausetNodeIdSet::GremlinNodeIdSet(gremlin_node_id_set) => {
+                gremlin_node_id_set.add(node_id.get_gremlin_node_id());
+            },
+            CausetNodeIdSet::FdbClusterNodeIdSet(fdb_cluster_node_id_set) => {
+                fdb_cluster_node_id_set.add(node_id.get_fdb_cluster_node_id());
+            },
+            CausetNodeIdSet::FdbDatabaseNodeIdSet(fdb_database_node_id_set) => {
+                fdb_database_node_id_set.add(node_id.get_fdb_database_node_id());
+            },
+            CausetNodeIdSet::PostgresNodeIdSet(postgres_node_id_set) => {
+                postgres_node_id_set.add(node_id.get_postgres_node_id());
+            },
         }
-    }
 
-    pub fn causal_set<R: Into<ValueRc<T>>>(&mut self, causet_locale: R) -> ValueRc<T> {
-        let soliton_id: ValueRc<T> = causet_locale.into();
-        if self.inner.insert(soliton_id.clone()) {
-            soliton_id
-        } else {
-            self.inner.get(&soliton_id).unwrap().clone()
-        }
+        use ValueRc;
+
+
+    /// An `CausalSet` allows to "causal_set" some potentially large causet_locales, maintaining a single causet_locale
+    /// instance owned by the `CausalSet` and leaving consumers with lightweight ref-counted handles to
+    /// the large owned causet_locale.  This can avoid expensive clone() operations.
+    ///
+    /// In EinsteinDB, such large causet_locales might be strings or arbitrary [a v] pairs.
+    ///
+    /// See https://en.wikipedia.org/wiki/String_causal_seting for discussion.
+    #[derive(Clone, Debug, Default, Eq, PartialEq)]
+    struct CausalSet<T> where T: Eq + Hash {
+        inner: HashSet<ValueRc<T>>,
+     }
     }
 }
+
+        impl<T> Deref for CausalSet<T> where T: Eq + Hash {
+            type Target = HashSet<ValueRc<T>>;
+
+            fn deref(&self) -> &Self::Target {
+                &self.inner
+            }
+        }
+
+        impl<T> DerefMut for CausalSet<T> where T: Eq + Hash {
+            fn deref_mut(&mut self) -> &mut Self::Target {
+                &mut self.inner
+            }
+        }
+
+        impl<T> CausalSet<T> where T: Eq + Hash {
+            pub fn new() -> CausalSet<T> {
+                CausalSet {
+                    inner: HashSet::new(),
+                }
+            }
+
+            pub fn causal_set<R: Into<ValueRc<T>>>(&mut self, causet_locale: R) -> ValueRc<T> {
+                let soliton_id: ValueRc<T> = causet_locale.into();
+                if self.inner.insert(soliton_id.clone()) {
+                    soliton_id
+                } else {
+                    self.inner.get(&soliton_id).unwrap().clone()
+                }
+            }
+        }
