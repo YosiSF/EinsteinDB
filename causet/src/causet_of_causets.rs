3ea72ed0b6e1684752e5d88972a853ad513ab4e8
Karl Whitford <karl@einsteindb.com>
2022-04-27 00:42:20 -0700

    (1) reuse fields from other types (2) avoid unused imports (3) avoid dead code warnings

--- causet/src/causet_of_causets.rs
+++ causet/src/causet_of_causets.rs
@@ -35,6 +35,19 @@ use std::fmt::Formatter;
 use std::hash::Hash;
 use std::hash::Hasher;
 
+use berolina_sql::{Sql, SqlType};
+use causet::causet::Causet;
+use causet::causet::Causet::{Causet, CausetOfCausets};
+
+use allegro_poset::{AllegroPoset, Poset};
+use rusqlite::{Connection, OpenFlags};
+use postgres_protocol::{Postgres, PostgresConnection};
+use foundationdb::{Fdb, FdbError, FdbResult};
+use foundationdb::{Fdb, FdbError, FdbResult};
+
+
+
+
 //implementation of the Causet of Causet
 
 //Allegro_Poset is an Allegro Lisp based off of the Partial Partition Poset of Causets  (POSET)
@@ -46,17 +59,119 @@ use std::hash::Hasher;
 //
 
 
+
+
+
+
+
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
 pub struct Causet<T> {
+
+    //foundationdb is a database that is a key value store
+    //      (see https://foundationdb.com/documentation/developer-guide.html)
+
+    //we need to be able to store the causet of causets in the database
+    //for this use Causet of Causets as the key
+    //and the solitonid of causets as the value for each event in the causet of causets
+
+    //the set of elements in the Causet
     pub elements: Vec<T>,
+
+    //we need planar geometry
+    //      (see https://en.wikipedia.org/wiki/Planar_graph)
+    pub planar_graph: Vec<Vec<usize>>,
+
+
+    //the set of causets that are in the Causet of Causets
+    //      (see https://en.wikipedia.org/wiki/Causet_of_causets)
+
+    pub causets: Vec<Causet<T>>,
+
+    //now we need an object to cache the foundationdb connection
+    #[allow(dead_code)]
+    pub fdb_connection: FdbConnection, //FdbConnection is a struct that is a connection to the foundationdb database
 }
+
+//lets work on that foundationdb connection; if there exists one then we can use it
+//if there does not exist one then we need to create one
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
+pub struct FdbConnection {
+    pub fdb: Fdb,
+    pub fdb_connection: FdbResult<PostgresConnection>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
+pub struct CausetOfCausets<T> {
+    pub causet_of_causets: Causet<T>,
+    pub causet_of_causets_of_causets: Causet<T>,
+}
+
 
 impl Causet<String> {
     pub fn new(elements: Vec<String>) -> Causet<String> {
+        //relativistic time
+        //      (see https://en.wikipedia.org/wiki/Relativistic_time)
+
+        let causet_of_causets = Causet::new(elements);
+        for causet in causet_of_causets.causets.iter() // iterate over the causets in the causet of causets
+        {
+            ///! we need to add the causet of causets to the causet of causets of causets index in the database
+            let causet_of_causets_of_causets = Causet::new(causet.elements.clone());
+            ///! now we instantiate the causet of causets of causets
+            causet.causets.push(causet_of_causets_of_causets);
+        }
         Causet {
             elements: elements,
+            planar_graph: vec![],
+            causets: causet_of_causets.causets,
+            fdb_connection: FdbConnection {
+                fdb: Fdb::new(),
+                fdb_connection: FdbResult::Ok(PostgresConnection::new("localhost", 5432, "postgres", "postgres", "postgres")),
+            },
         }
     }
+
+    pub fn add_causet(&mut self, causet: Causet<String>) {
+        self.causets.push(causet);
+    }
+
+    pub fn add_causet_of_causets(&mut self, causet_of_causets: Causet<String>) {
+        self.causets.push(causet_of_causets);
+    }
+
+
+    pub fn add_causet_of_causets_of_causets(&mut self, causet_of_causets_of_causets: Causet<String>) {
+        self.causets.push(causet_of_causets_of_causets);
+    }
+
+
+    pub fn add_causet_of_causets_of_k8s(&mut self, causet_of_causets_of_causets_of_causets: Causet<String>) {
+
+        //k8s
+        //      (see https://en.wikipedia.org/wiki/Kubernetes)
+
+        let k8s_causet = Causet::new(vec!["k8s".to_string()]);
+
+        let k8s_solitonid_fetch = self.fdb_connection.fdb_connection.unwrap().query("SELECT solitonid FROM causet WHERE elements = 'k8s'", &[]).unwrap();
+
+        fn get_solitonid_from_row(row: &Row) -> i32 {
+            row.get(0)
+        }
+
+        Causet {
+            elements,
+            planar_graph: (),
+            causets: (),
+            fdb_connection: FdbConnection {
+                //enter kubernetes cluster
+                fdb: Fdb::new(),
+                fdb_connection: FdbResult::Ok(PostgresConnection::new("foundationdb://
+                ").unwrap()),
+            }, //enter kubernetes cluster
+        }
+    }
+
 
     pub fn new_from_vec(elements: Vec<&str>) -> Causet<String> {
         let mut new_elements = Vec::new();
@@ -65,9 +180,12 @@ impl Causet<String> {
         }
         Causet {
             elements: new_elements,
+            planar_graph: (),
+            causets: (),
+            fdb_connection: FdbConnection { fdb: (), fdb_connection: () }
         }
     }
-
+}
     pub fn new_from_vec_ref(elements: Vec<&str>) -> Causet<String> {
         let mut new_elements = Vec::new();
         for element in elements {
@@ -75,6 +193,9 @@ impl Causet<String> {
         }
         Causet {
             elements: new_elements,
+            planar_graph: (),
+            causets: (),
+            fdb_connection: FdbConnection { fdb: (), fdb_connection: () }
         }
     }
 
@@ -85,18 +206,12 @@ impl Causet<String> {
         }
         Causet {
             elements: new_elements,
+            planar_graph: (),
+            causets: (),
+            fdb_connection: FdbConnection { fdb: (), fdb_connection: () }
         }
     }
 
-    pub fn new_from_vec_ref_mut_mut(elements: Vec<&str>) -> Causet<String> {
-        let mut new_elements = Vec::new();
-        for element in elements {
-            new_elements.push(element.to_string());
-        }
-        Causet {
-            elements: new_elements,
-        }
-    }
 
     pub fn new_from_vec_ref_mut_mut_mut(elements: Vec<&str>) -> Causet<String> {
         let mut new_elements = Vec::new();
@@ -105,31 +220,30 @@ impl Causet<String> {
         }
         Causet {
             elements: new_elements,
+            planar_graph: (),
+            causets: (),
+            fdb_connection: FdbConnection { fdb: (), fdb_connection: () }
         }
     }
 
-    pub fn new_from_vec_ref_mut_mut_mut_mut(elements: Vec<&str>) -> Causet<String> {
-        let mut new_elements = Vec::new();
-        for element in elements {
-            new_elements.push(element.to_string());
-        }
-        Causet {
-            elements: new_elements,
-        }
-    }
-    //constructor
 
-    //constructor
+    ///! TODO: implement this with_capacity method to bin the elements in the causet
     pub fn with_capacity(capacity: usize) -> Causet<T> {
         Causet {
             elements: Vec::with_capacity(capacity),
+            planar_graph: (),
+            causets: (),
+            fdb_connection: FdbConnection { fdb: (), fdb_connection: () }
         }
     }
 
     //constructor
     pub fn with_elements(elements: Vec<T>) -> Causet<T> {
         Causet {
-            elements: elements,
+            elements,
+            planar_graph: (),
+            causets: (),
+            fdb_connection: FdbConnection { fdb: (), fdb_connection: () }
         }
     }
 
@@ -137,16 +251,22 @@ impl Causet<String> {
     pub fn with_element(element: T) -> Causet<T> {
         Causet {
             elements: vec![element],
+            planar_graph: (),
+            causets: (),
+            fdb_connection: FdbConnection { fdb: (), fdb_connection: () }
         }
     }
 
     //constructor
     pub fn with_elements_and_capacity(elements: Vec<T>, capacity: usize) -> Causet<T> {
         Causet {
-            elements: elements,
+            elements,
+            planar_graph: (),
+            causets: (),
+            fdb_connection: FdbConnection { fdb: (), fdb_connection: () }
         }
     }
-}
+
 
 
 
--- causet/src/causetxctx_control_factors.rs
+++ causet/src/causetxctx_control_factors.rs
@@ -1,18 +1,34 @@
 // Copyright 2021 EinsteinDB Project Authors. Licensed under Apache-2.0.
+//! # CausetXContext Control Factors
+//! This module contains the control factors for CausetXContext.
+//! The control factors are used to control the behavior of the CausetXContext.
+//! The control factors are used to control the behavior of the CausetXContext.
+//!
+//!
+//!
+//! ## CausetXContext Control Factors
+//!
+//!
+ pub use self::causetxctx_control_factors::*;
+#[cfg(any(test,feature = "test"))]
 
 
 use super::*;
 use crate::errors::Result;
+use crate::metrics::*;
 
+///! CausetXContext Control Factors
 pub trait SymplecticControlFactorsExt {
+    ///! CausetXContext Control Factors
     fn new(
-        &self,
-        control_factors: &[f64],
+        // for CausetXContext we use the default values
+        &self, // self is the CausetXContext for which we are creating the control factors
+        control_factors: &[f64], //a control factor for each of the control factors
         control_factors_derivative: &[f64],
         control_factors_derivative_2: &[f64],
     ) -> Result<Self>
-    where
-        Self: Sized;
+        where
+            Self: Sized;
     ///
     ///
     /// # Arguments
@@ -25,14 +41,67 @@ pub trait SymplecticControlFactorsExt {
     /// # Examples
     ///
     /// ```
+
+
+
     ///
-    /// ```
     ///
-    /// # Errors
-    ///
-    ///
-    /// # Safety
-    fn get_namespaced_num_fuse_at_l_naught(&self, namespaced: &str, l_naught: usize) -> Result<Option<u64>>;
+
+
+    async fn get_control_factors<'a>(&'a self,
+                                     control_factors: &'a [f64],
+                                     control_factors_derivative: &'a [f64],
+                                     control_factors_derivative_2: &'a [f64],
+    ) -> Result<(Vec<f64>, Vec<f64>, Vec<f64>)>
+        where
+            Self: Sized;
+
+}
+
+
+
+
+impl SymplecticControlFactorsExt for CausetXContext {
+    fn get_namespaced_num_fuse_at_l_naught(&self, namespaced: &str, l_naught: usize) -> Result<Option<u64>> {
+        if {
+            let mut namespaced_num_fuse_at_l_naught = self.namespaced_num_fuse_at_l_naught.lock().unwrap();
+            namespaced_num_fuse_at_l_naught.contains_key(namespaced);
+
+            namespaced_num_fuse_at_l_naught.get(namespaced).unwrap().contains_key(&l_naught);
+        } {
+            let mut namespaced_num_fuse_at_l_naught = self.namespaced_num_fuse_at_l_naught.lock().unwrap();
+            Ok(namespaced_num_fuse_at_l_naught.get(namespaced).unwrap().get(&l_naught).unwrap().clone())
+        } else {
+            Ok(None)
+        }
+    }
+
+    fn get_namespaced_num_fuse_at_l_naught_derivative(&self) -> Result<Option<f64>> {
+        if {
+            let mut namespaced_num_fuse_at_l_naught_derivative = self.namespaced_num_fuse_at_l_naught_derivative.lock().unwrap();
+            namespaced_num_fuse_at_l_naught_derivative.contains_key("");
+
+            namespaced_num_fuse_at_l_naught_derivative.get("").unwrap().contains_key(&0);
+        } {
+            let mut namespaced_num_fuse_at_l_naught_derivative = self.namespaced_num_fuse_at_l_naught_derivative.lock().unwrap();
+            Ok(namespaced_num_fuse_at_l_naught_derivative.get("").unwrap().get(&0).unwrap().clone())
+        }
+    }
+
+    //dedup
+
+    fn get_namespaced_num_fuse_at_l_naught_derivative_2(&self) -> Result<Option<f64>> {
+        if {
+            let mut namespaced_num_fuse_at_l_naught_derivative_2 = self.namespaced_num_fuse_at_l_naught_derivative_2.lock().unwrap();
+            namespaced_num_fuse_at_l_naught_derivative_2.contains_key("");
+
+            namespaced_num_fuse_at_l_naught_derivative_2.get("").unwrap().contains_key(&0);
+        } {
+            let mut namespaced_num_fuse_at_l_naught_derivative_2 = self.namespaced_num_fuse_at_l_naught_derivative_2.lock().unwrap();
+            Ok(namespaced_num_fuse_at_l_naught_derivative_2.get("").unwrap().get(&0).unwrap().clone())
+        }
+    }
+
 
     ///
     /// Returns the number of fuse at the given level.
@@ -44,11 +113,15 @@ pub trait SymplecticControlFactorsExt {
     ///     The level is zero-based.
     ///     The level is the number of times the symplectic matrix has been applied.
     ///
-    fn get_namespaced_num_immutable_mem_table(&self, namespaced: &str) -> Result<Option<u64>>;
+    fn get_namespaced_num_immutable_mem_table(&self, namespaced: &str) -> Result<Option<u64>>{
+        Ok(None)
+    }
 
 
     ///
-    fn get_namespaced_pending_jet_bundle_bytes(&self, namespaced: &str) -> Result<Option<u64>>;
+    fn get_namespaced_pending_jet_bundle_bytes(&self, namespaced: &str) -> Result<Option<u64>>{
+        Ok(None)
+    }
 }
 
 
