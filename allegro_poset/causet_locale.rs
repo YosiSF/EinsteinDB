4ade7bdf54ba0e07ff60bbcb7efd43243e1f3c42
Karl Whitford <karl@einsteindb.com>
2022-07-12 01:52:27 -0700

    k8s solitonid_range

--- allegro_poset/causet_locale.rs
+++ allegro_poset/causet_locale.rs
@@ -1,5 +1,6 @@
 // Whtcorps Inc 2022 Apache 2.0 License; All Rights Reserved.
 //
+
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use
 // this file File except in compliance with the License. You may obtain a copy of the
 // License at http://www.apache.org/licenses/LICENSE-2.0
